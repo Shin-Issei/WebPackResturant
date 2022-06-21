d59756cd3cce4bdae7e18b8bae19b21a656fcd88
Jordan <JMCollins0@aol.com>
2022-06-21 12:07:44 -0400

    Prettier format + import style sheet

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@
 
 const HomeBtn = document.createElement("button");
 const main = document.createElement("div"); // Append all function results to main
-main.classList.add("main")
+main.classList.add("main");
+const Tabs = document.createElement("div"); //
+Tabs.classList.add("tabs");
 
 HomeBtn.textContent = "Home";
-HomeBtn.addEventListener("click", ()=>{
-  main.textContent = "A blurb about how great the resturant is."
+HomeBtn.addEventListener("click", () => {
+  main.textContent = "A blurb about how great the resturant is.";
 });
 
-
 document.body.appendChild(main);
-document.body.appendChild(HomeBtn);
+document.body.appendChild(Tabs);
+Tabs.appendChild(HomeBtn);
